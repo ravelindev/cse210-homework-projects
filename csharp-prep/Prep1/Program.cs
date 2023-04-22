4bcec54be3ae823e28b4ef27bf1c65a69592c196
ravelindev <ravelindev@gmail.com>
2023-04-22 13:41:27 -0700

    Done with Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt the user for their first name
+        Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();
+
+        // Prompt the user for their last name
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+
+        // Display the name in the desired format
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
