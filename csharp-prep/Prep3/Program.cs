208d20daddb642cffade5be4023b5466568c872a
ravelindev <ravelindev@gmail.com>
2023-04-25 20:16:36 -0700

    Using an if statement, determine if the user needs to guess higher or lower next time, or tell them if they guessed it

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        if (number > guess)
+        {
+            Console.WriteLine("Higher");
+
+        }
+        else if(number < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guess it!");
+        }
+
     }
 }
