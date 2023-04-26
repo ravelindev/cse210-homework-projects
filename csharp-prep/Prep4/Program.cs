bc0d34393df1c81f07e777ec7dffbc78fa3627d8
ravelindev <ravelindev@gmail.com>
2023-04-25 21:43:00 -0700

    Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Enter a number (enter 0 to stop): ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+            {
+                break;
+            }
+            numbers.Add(num);
+        }
+
+        Console.WriteLine("List of numbers: ");
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        double average = (double)sum / numbers.Count;
+
+        int maximum = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > maximum)
+            {
+                maximum = num;
+            }
+        }
+
+        Console.WriteLine("\nSum: " + sum);
+        Console.WriteLine("Average: " + average);
+        Console.WriteLine("Maximum: " + maximum);
     }
 }
